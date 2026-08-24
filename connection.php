4d74b3fabb5864e2b541e985d3e5d8586022e1cb
shahzeen bugti <shehzeenliaquat0@gmail.com>
2026-08-23 17:08:09 -0700

    Add database connection script in connection.php

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "student";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Database Connected Successfully";
+?>
+
+
